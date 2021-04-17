11f438f0b9bd52d73b6ca2d96ceddcbbcfa54872
Daghdha <daghdha1.dev@gmail.com>
2021-04-17 14:02:13 +0200

    BASE --> Ahora todos los paquetes están incluidos en navigation_system

--- navigation_system/src/call_map_service.py
+++ navigation_system/src/call_map_service.py
@@ -3,7 +3,7 @@
 import rospy
 from nav_msgs.srv import GetMap, GetMapRequest
 
-rospy.init_node('service_client') # se inicializa el nodo cliente
+rospy.init_node('map_client') # se inicializa el nodo cliente
 rospy.wait_for_service('/static_map') # esperamos a que el servicio este activo
 get_map_service = rospy.ServiceProxy('/static_map', GetMap)  # se crea la conexion al servicio
 get_map = GetMapRequest() # se crea un objeto del tipo GetMapRequest
